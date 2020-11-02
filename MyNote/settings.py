90d08c1c4fa977889e0bca78be7ee14ca48a52f0
Patryk Szmekel <patryk.szmekel@gmail.com>
2020-11-02 11:44:18 +0100

    add CustoUser model

--- MyNote/settings.py
+++ MyNote/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',  # Custom User
 ]
 
 MIDDLEWARE = [
@@ -111,3 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Custom User
+AUTH_USER_MODEL = 'myapp.CustomUser'
