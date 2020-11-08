a8f08b2400229d307a5bba0ba1d1729ff18a4d10
Patryk Szmekel <patryk.szmekel@gmail.com>
2020-11-08 15:45:38 +0100

    add more test for accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url, include
+from django.conf.urls import include
+from django.urls import re_path
 from rest_framework import routers
 from accounts import views
 
@@ -6,5 +7,5 @@ router = routers.DefaultRouter()
 router.register(r'users', views.CustomUserViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    re_path(r'^', include(router.urls)),
 ]
--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -1,8 +1,11 @@
 import pytest
 
 from django.db.utils import IntegrityError
+from rest_framework.test import APIClient
 
 from accounts.models import CustomUser
+
+# User
 
 
 @pytest.mark.django_db
@@ -21,6 +24,22 @@ def test_user_create_unique():
     except(IntegrityError):
         is_unique = False
     assert is_unique == False
+
+
+@pytest.mark.django_db
+def test_user_create_pass_ok():
+    user = CustomUser.objects.create_user(
+        'lennon@thebeatles.com', 'johnpassword')
+    assert user.check_password('johnpassword')
+
+
+@pytest.mark.django_db
+def test_user_create_pass_not_ok():
+    user = CustomUser.objects.create_user(
+        'lennon@thebeatles.com', 'johnpassword')
+    assert user.check_password('johnpassworda') == False
+
+# Super User
 
 
 @pytest.mark.django_db
@@ -43,3 +62,54 @@ def test_superuser_create_unique():
     except(IntegrityError):
         is_unique = False
     assert is_unique == False
+
+# API
+
+
+@pytest.mark.django_db
+def test_get_users():
+    CustomUser.objects.create_user('lennon@thebeatles.com', 'johnpassword')
+    client = APIClient()
+    response = client.get('/api/users/')
+    assert response.status_code == 200
+    assert response.data != []
+
+
+@pytest.mark.django_db
+def test_get_superuser():
+    CustomUser.objects.create_superuser(
+        'lennon@thebeatles.com', 'johnpassword')
+    client = APIClient()
+    response = client.get('/api/users/')
+    assert response.data == []  # can't get superuser by api, dataset shoudl be empty
+
+
+@pytest.mark.django_db
+def test_post_user():
+    client = APIClient()
+    response = client.post(
+        '/api/users/', {'email': 'test@mail.com', 'password': 'password'})
+    assert response.status_code == 201
+    assert CustomUser.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_put_user():
+    user = CustomUser.objects.create_user(
+        'lennon@thebeatles.com', 'johnpassword')
+    client = APIClient()
+    response = client.put(
+        '/api/users/' + str(user.id) + '/', {'email': 'test1@mail.com', 'password': 'password1'})
+    assert response.status_code == 200
+    user.refresh_from_db()
+    assert user.email == 'test1@mail.com'
+
+
+@pytest.mark.django_db
+def test_delete_user():
+    user = CustomUser.objects.create_user(
+        'lennon@thebeatles.com', 'johnpassword')
+    client = APIClient()
+    response = client.delete('/api/users/' + str(user.id) + '/')
+    assert response.status_code == 204
+    assert CustomUser.objects.count() == 0
